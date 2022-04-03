1e9aeccbaa2af4ee3501a82fbcc53c7dc2b1aa9d
HebbyHebz <42718288+HebbyHebz@users.noreply.github.com>
2022-04-03 09:59:04 +0000

    Initial State

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-def greeting(name )
-n
+def seconds(number):
+    hours = number // 3600
+    minutes = (number - (hours * 3600))//60
+    seconds  = number - ((hours * 3600) + (minutes*60))
+
+    return hours, minutes, seconds;
+
+
+print(seconds(10800))
+
+h, m, s = seconds(7000)
+print(h,m,s)
